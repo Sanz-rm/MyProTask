9193a5469f7ef1d7f13461c7ae1a0ef74fffbc5a
Sanz-rm <sanzromerom@gmail.com>
2024-07-22 00:38:04 +0200

    MyProTask Final

---
+++ MyProTaskBack/myprotask/src/main/java/com/app/myprotask/model/enums/PriorityTasks.java
@@ -0,0 +1,12 @@
+package com.app.myprotask.model.enums;
+
+/**
+ * Priority tasks options
+ * 
+ * @author Alejandro
+ */
+public enum PriorityTasks {
+	HIGH, MID, LOW
+}
+
+
---
+++ MyProTaskFront/MyProTask/src/app/servicios/auth/jwt.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/servicios/auth/auth.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Añadir el encabezado de autorización con el token JWT si está disponible
+    const token = localStorage.getItem('loginToken');
+    if (this.authService.isLoggedIn && token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
